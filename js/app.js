44c07ea6a2ba4182672a7994e713b51230bb9155
siriuusgroup <168030281+siriuusgroup@users.noreply.github.com>
2024-06-10 20:14:36 -0300

    javascript

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const colors = ["1a237e", "black", "#0091ea", "#4a148c", "#006064", "#263238"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
